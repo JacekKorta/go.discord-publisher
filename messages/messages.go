deacf39e44224a209c31f14c3bc01b47d81d961f
JacekKorta <jacek.korta@gmail.com>
2022-10-09 21:13:25 +0200

    Add messages

---
+++ messages/messages.go
@@ -0,0 +1,26 @@
+package messages
+
+
+type QuestionIn struct {
+	Tags             []string `json:"tags"`
+	IsAnswered       bool     `json:"is_answered"`
+	LastActivityDate int      `json:"last_activity_date"`
+	CreationDate     int      `json:"creation_date"`
+	QuestionID       int      `json:"question_id"`
+	Link             string   `json:"link"`
+	Title            string   `json:"title"`
+	Body             string   `json:"body"`
+	Reasons []string `json:"reasons"`
+}
+
+
+type Embed struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+	
+type DiscordMessageOut struct {
+	Content string `json:"content"`
+	Tts     bool   `json:"tts"`
+	Embeds  []Embed `json:"embeds"`
+}
